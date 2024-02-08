e32a8d5c5a4d55c9f9c47695b0f4c4514677401a
Tanish Pandey <tanishpandey14@gmail.com>
2024-02-08 19:41:28 +0530

    data fetch from api

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  const 
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Data.jsx
@@ -0,0 +1,30 @@
+function Data(props){
+    console.log("Data Component",Object.keys(props.data[0]))
+    return(
+        <>
+        <table className="table">
+            <thead>
+                <tr>
+                    {Object.keys(props.data[0]).map((key)=> (
+                        <th scope="col">{key}</th>
+                    ))}
+
+                </tr>
+            </thead>
+            <tbody>
+                {props.data.map((data)=>(
+                    <tr>
+                        {Object.keys(data).map((key)=>(
+                            typeof data[key] === 'object' ?
+                            <td>xyz</td>
+                            :
+                            <td>{data[key]}</td>
+                        )
+                        )}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        </>
+    );
+}
